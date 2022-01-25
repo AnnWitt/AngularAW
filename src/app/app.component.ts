e4963fb1cbec27b5772dab1645bab092ee179b92
Anna Wittmann <annwitt@tlen.pl>
2022-01-26 00:56:52 +0100

    Szkielet v1

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'AW-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AnnWitt';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { TableComponent } from './table/table.component';
+import { ArticleComponent } from './article/article.component';
+import { PlaygroundComponent } from './playground/playground.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    TableComponent,
+    ArticleComponent,
+    PlaygroundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
