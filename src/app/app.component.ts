04ec8fab006b70266651b820d6781fb5f8a767a8
Anna Wittmann <annwitt@tlen.pl>
2022-01-28 02:37:44 +0100

    7c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'AW-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AnnWitt';
+  item='DUUUUPAAAA Input z komponentu app component do komponentu article';
 }
+
+// @Input() lets a parent component update data in the child component. Conversely, 
+// @Output() lets the child send data to a parent component.
+// The @Input() decorator in a child component or directive signifies that the
+// property can receive its value from its parent component.
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'AW-article',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @Input() itemArticle='';
 
-  constructor() { }
+  constructor() {
+   }
+
+  annestokes: string="Jej ilustracje trafiły na koszulki wielu zespołów, a t-shirt ze wzorem Skull Tatoo pojawił się w filmie Tima Burtona \"Charlie i fabryka czekolady\". Jej pozycję ugruntowały zlecenia na wykonanie szeregu concept artów i ilustracji dla Wizards of the Coast czy Dungeons & Dragons. Obecnie tworzy własne, licencjonowane wzory dostępne w szerokiej gamie produktów takich jak kubki, koszulki, plakaty, rzeźby, okładki książek, kalendarze, układanki, karty, biżuteria i pocztówki.";
+
+   
 
   ngOnInit(): void {
+    
   }
 
 }
